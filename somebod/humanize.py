06b2d4f8ab912783a27a5a62aa996572b6c68ce2
Guillaume Thomas <guillaume.thomas642@gmail.com>
2014-12-27 10:56:44 +0100

    added parse duration method

--- somebod/humanize.py
+++ somebod/humanize.py
@@ -3,6 +3,8 @@
 import decimal
 
 intword_converters = ((1E15, "P"), (1E12, "T"), (1E9, "G"), (1E6, "M"), (1E3, "K"))
+duration_suffixes = dict((("s", 1), ("m", 60), ("h", 60 * 60), ("d", 24 * 60 * 60),
+                         ("w", 7 * 24 * 60 * 60), ("y", 365 * 7 * 24 * 60 * 60)))
 
 
 def intword(val):
@@ -31,3 +33,16 @@ def print_ratio(value, den):
         Human output of a ratio
     """
     return "%s/%s (%.2f%%)" % (intword(value), intword(den), 100. * value / den if den else 0.)
+
+
+def parse_duration(duration):
+    """
+        Parse human duration into duration in seconds
+    """
+    if not isinstance(duration, str) and not isinstance(duration, unicode):
+        raise TypeError("Cannot parse duration. Must be string or unicode")
+    if duration.isdigit():
+        return int(duration)
+    suffix = duration[-1]
+    prefix = duration[:-1]
+    return int(prefix) * duration_suffixes[suffix]
